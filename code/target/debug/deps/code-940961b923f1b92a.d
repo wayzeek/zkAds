bd4362960456be784197496b85c2f2afefdf86c4
vcart <112975047+wayzeek@users.noreply.github.com>
2024-08-10 15:59:30 -0400

    add: adding basic distance calculation program

---
+++ code/target/debug/deps/code-940961b923f1b92a.d
@@ -0,0 +1,5 @@
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/libcode-940961b923f1b92a.rmeta: src/main.rs
+
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/code-940961b923f1b92a.d: src/main.rs
+
+src/main.rs:
