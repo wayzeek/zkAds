4906c17a0623c78a7886e9664b17952f6a3b93d5
AntoineF4C5 <antoinedcht@hotmail.com>
2024-08-10 17:50:20 -0400

    using default schema to generate proof using zkEngine

--- zkv/src/main.rs
+++ zkv/src/main.rs
@@ -1,25 +1,54 @@
-  use std::path::PathBuf;
-
-  use zk_engine::{
+use std::path::PathBuf;
+// Backend imports
+use zk_engine::nova::{
+    provider::{ipa_pc, PallasEngine},
+    spartan::{self, snark::RelaxedR1CSSNARK},
+    traits::{
+        snark::{BatchedRelaxedR1CSSNARKTrait, RelaxedR1CSSNARKTrait},
+        CurveCycleEquipped, Dual, Engine,
+    },
+};
+use zk_engine::{
     args::{WASMArgsBuilder, WASMCtx},
-    run::prove_execution,
+    run::default::ZKEProof,
+    traits::zkvm::ZKVM,
     utils::logging::init_logger,
-  };
+};
 
-  fn main() -> anyhow::Result<()> {
+// Curve cycle to use for proving
+type E1 = PallasEngine;
+// PCS used for final SNARK at the end of (N)IVC
+type EE1<E> = ipa_pc::EvaluationEngine<E>;
+// PCS on secondary curve
+type EE2<E> = ipa_pc::EvaluationEngine<Dual<E>>;
+
+// Spartan SNARKS used for compressing at then end of (N)IVC
+type BS1<E> = spartan::batched::BatchedRelaxedR1CSSNARK<E, EE1<E>>;
+type S1<E> = RelaxedR1CSSNARK<E, EE1<E>>;
+type S2<E> = RelaxedR1CSSNARK<Dual<E>, EE2<E>>;
+
+fn main() -> anyhow::Result<()> {
     init_logger();
 
     // Configure the arguments needed for WASM execution
     //
     // Here we are configuring the path to the WASM file
     let args = WASMArgsBuilder::default()
-      .file_path(PathBuf::from("../code/target/wasm32-wasi/release/code.wasm"))
-      .invoke(Some(String::from("__main_void")))
-      .build();
+        .file_path(PathBuf::from("wasm/example.wasm"))
+        .build();
 
     // Create a WASM execution context for proving.
     let mut wasm_ctx = WASMCtx::new_from_file(args)?;
 
-    let batched_config = true;
-    prove_execution(&mut wasm_ctx, batched_config)
+    // Prove execution and run memory consistency checks
+    //
+    // Get proof for verification and corresponding public values
+    //
+    // Above type alias's (for the backend config) get used here
+    let (proof, public_values) =
+        ZKEProof::<E1, BS1<E1>, S1<E1>, S2<E1>>::prove_wasm(&mut wasm_ctx)?;
+
+    // Verify proof
+    let result = proof.verify(public_values)?;
+    Ok(assert!(result))
 }
