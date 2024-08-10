c174fb2b1a2f824ef97977baab416a3ae90a71c9
vcart <vcart@student.42lyon.fr>
2024-08-10 16:05:30 -0400

    add: wasm file

---
+++ code/target/wasm32-wasi/release/code.d
@@ -0,0 +1,1 @@
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/wasm32-wasi/release/code.wasm: /home/vcart/own_sh*t/zkHack/zkAds/code/src/main.rs
---
+++ code/target/wasm32-wasi/release/deps/code-8ec3142468c6b725.d
@@ -0,0 +1,5 @@
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/wasm32-wasi/release/deps/code-8ec3142468c6b725.wasm: src/main.rs
+
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/wasm32-wasi/release/deps/code-8ec3142468c6b725.d: src/main.rs
+
+src/main.rs:
