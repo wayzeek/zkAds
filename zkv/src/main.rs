a69ce6a0f84d031ad02043504a7e4be13fb4000b
vcart <vcart@student.42lyon.fr>
2024-08-10 19:09:58 -0400

    add: saving proof in folder

--- zkv/src/main.rs
+++ zkv/src/main.rs
@@ -1,54 +1,84 @@
+#![allow(unused)]
+
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::Write;
 // Backend imports
-use zk_engine::nova::{
+use serde::Deserialize;
+use zk_engine::{
+  args::{WASMArgsBuilder, WASMCtx},
+  nova::{
     provider::{ipa_pc, PallasEngine},
     spartan::{self, snark::RelaxedR1CSSNARK},
     traits::{
-        snark::{BatchedRelaxedR1CSSNARKTrait, RelaxedR1CSSNARKTrait},
-        CurveCycleEquipped, Dual, Engine,
+      snark::{BatchedRelaxedR1CSSNARKTrait, RelaxedR1CSSNARKTrait},
+      CurveCycleEquipped, Dual, Engine,
     },
-};
-use zk_engine::{
-    args::{WASMArgsBuilder, WASMCtx},
-    run::default::ZKEProof,
-    traits::zkvm::ZKVM,
-    utils::logging::init_logger,
+  },
+  run::{
+    batched::{BatchedZKEExecutionProof, BatchedZKEProof},
+    prove_execution_batched,
+  },
+  traits::zkvm::ZKVM,
+  utils::logging::init_logger,
+  BatchedExecutionProof, BatchedExecutionPublicParams, ExecutionPublicValues,
+  SuperNovaPublicParams,
 };
 
-// Curve cycle to use for proving
+// Backend configs
 type E1 = PallasEngine;
-// PCS used for final SNARK at the end of (N)IVC
 type EE1<E> = ipa_pc::EvaluationEngine<E>;
-// PCS on secondary curve
 type EE2<E> = ipa_pc::EvaluationEngine<Dual<E>>;
-
-// Spartan SNARKS used for compressing at then end of (N)IVC
 type BS1<E> = spartan::batched::BatchedRelaxedR1CSSNARK<E, EE1<E>>;
 type S1<E> = RelaxedR1CSSNARK<E, EE1<E>>;
 type S2<E> = RelaxedR1CSSNARK<Dual<E>, EE2<E>>;
 
 fn main() -> anyhow::Result<()> {
-    init_logger();
+  init_logger();
 
-    // Configure the arguments needed for WASM execution
-    //
-    // Here we are configuring the path to the WASM file
-    let args = WASMArgsBuilder::default()
-        .file_path(PathBuf::from("wasm/example.wasm"))
-        .build();
+  // Some WASM' modules require the function to invoke and it's functions arguments.
+  // The below code is an example of how to configure the WASM arguments for such cases.
+  //
+  // This WASM module (fib.wat) has a fib fn which will
+  // produce the n'th number in the fibonacci sequence.
+  // The function we want to invoke has the following signature:
+  //
+  // fib(n: i32) -> i32;
+  //
+  // This means the higher the user input is for `n` the more opcodes will need to be proven
+  let args = WASMArgsBuilder::default()
+    .file_path(PathBuf::from("../code/target/wasm32-wasi/release/code.wasm"))
+    .invoke(Some(String::from("__main_void")))
+    .func_args(vec![String::from("1000")]) // This will generate 16,000 + opcodes
+    .build();
+  let mut wasm_ctx = WASMCtx::new_from_file(args)?;
 
-    // Create a WASM execution context for proving.
-    let mut wasm_ctx = WASMCtx::new_from_file(args)?;
+  // json-serialized proof and public values
+  let (proof, pp, po, pi) = prove_execution_batched(&mut wasm_ctx)?;
 
-    // Prove execution and run memory consistency checks
-    //
-    // Get proof for verification and corresponding public values
-    //
-    // Above type alias's (for the backend config) get used here
-    let (proof, public_values) =
-        ZKEProof::<E1, BS1<E1>, S1<E1>, S2<E1>>::prove_wasm(&mut wasm_ctx)?;
+  // Save the serialized proof and public parameters to files
+  save_to_file("proof/proof.json", &proof)?;
+  save_to_file("proof/public_parameters.json", &pp)?;
+  save_to_file("proof/po.json", &po)?;
+  save_to_file("proof/pi.json", &pi)?;
 
-    // Verify proof
-    let result = proof.verify(public_values)?;
-    Ok(assert!(result))
+  // deserialize proof and public parameters
+  let proof: BatchedExecutionProof<E1, BS1<E1>, S2<E1>> = serde_json::from_str(&proof).unwrap();
+  let pp: SuperNovaPublicParams<E1> = serde_json::from_str(&pp).unwrap();
+  let po: Vec<<PallasEngine as Engine>::Scalar> = serde_json::from_str(&po).unwrap();
+  let pi: Vec<<PallasEngine as Engine>::Scalar> = serde_json::from_str(&pi).unwrap();
+
+  let proof = BatchedZKEExecutionProof::<E1, BS1<E1>, S1<E1>, S2<E1>>::new(proof);
+  let public_params = BatchedExecutionPublicParams::<E1, BS1<E1>, S2<E1>>::from(pp);
+  let public_values = ExecutionPublicValues::new(public_params, &po, &pi);
+
+  let result = proof.verify_wasm_execution(public_values)?;
+  Ok(assert!(result))
+}
+
+fn save_to_file(filename: &str, data: &str) -> anyhow::Result<()> {
+  let mut file = File::create(filename)?;
+  file.write_all(data.as_bytes())?;
+  println!("Data written to {}", filename);
+  Ok(())
 }
