2556f7c83f43472972ca055ce9cdf0549a9962a4
vcart <vcart@student.42lyon.fr>
2024-08-10 15:59:30 -0400

    add: adding basic distance calculation program

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,3 +1,42 @@
+struct User {
+    name: String,
+    x: f32,
+    y: f32,
+}
+
+fn calculate_distance(user1: &User, user2: &User) -> f32 {
+    let dx = user2.x - user1.x;
+    let dy = user2.y - user1.y;
+    (dx * dx + dy * dy).sqrt()
+}
+
+fn find_nearest_company(user: User, mut companies: Vec<User>) -> User {
+    if companies.len() < 2 {
+        companies.remove(0) // remove returns the element, taking ownership
+    } else {
+        let mut smallest_distance = calculate_distance(&user, &companies[0]);
+        let mut result_index = 0;
+
+        for (i, company) in companies.iter().enumerate() {
+            let distance = calculate_distance(&user, company);
+            if distance < smallest_distance {
+                smallest_distance = distance;
+                result_index = i;
+            }
+        }
+
+        companies.remove(result_index) // remove returns the element, taking ownership
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User { name: String::from("Valentin"), x: 42.42, y: 56.30 };
+    
+    let company1 = User { name: String::from("Ubisoft"), x: 40.42, y: 56.30 };
+    let company2 = User { name: String::from("Google"), x: -42.42, y: 56.30 };
+
+    let companies = vec![company1, company2];
+
+    let nearest_company = find_nearest_company(user1, companies);
+    println!("Nearest company is {} at: ({}, {})", nearest_company.name, nearest_company.x, nearest_company.y);
 }
---
+++ code/target/debug/deps/code-988d39ebea653a9c.d
@@ -0,0 +1,5 @@
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/code-988d39ebea653a9c: src/main.rs
+
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/code-988d39ebea653a9c.d: src/main.rs
+
+src/main.rs:
