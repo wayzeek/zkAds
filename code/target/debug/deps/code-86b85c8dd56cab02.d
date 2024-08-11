23a78f90b3211a71920b831012ace7fc86e60c88
AntoineF4C5 <antoinedcht@hotmail.com>
2024-08-10 20:39:20 -0400

    added code/target to gitignore

--- code/target/debug/deps/code-86b85c8dd56cab02.d
+++
@@ -1,5 +0,0 @@
-/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/libcode-86b85c8dd56cab02.rmeta: src/main.rs
-
-/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/code-86b85c8dd56cab02.d: src/main.rs
-
-src/main.rs:
--- code/target/wasm32-wasi/release/code.d
+++
@@ -1,1 +0,0 @@
-/home/antoine/Documents/projets/dev/zkHack/project/zkAds/code/target/wasm32-wasi/release/code.wasm: /home/antoine/Documents/projets/dev/zkHack/project/zkAds/code/src/main.rs
