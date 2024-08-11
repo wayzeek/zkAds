55f5da4e495f91c4a3f9a8967908cb0a60fae9e8
vcart <112975047+wayzeek@users.noreply.github.com>
2024-08-10 21:35:53 -0400

    add: changed rust implementation to match WASM requirements

--- code/target/debug/deps/code-f2251c457d0032f7.d
+++ code/target/debug/deps/code-f2251c457d0032f7.d
@@ -1,5 +1,5 @@
-/home/antoine/Documents/projets/dev/zkHack/project/zkAds/code/target/debug/deps/libcode-f2251c457d0032f7.rmeta: src/main.rs
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/libcode-f2251c457d0032f7.rmeta: src/main.rs
 
-/home/antoine/Documents/projets/dev/zkHack/project/zkAds/code/target/debug/deps/code-f2251c457d0032f7.d: src/main.rs
+/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/deps/code-f2251c457d0032f7.d: src/main.rs
 
 src/main.rs:
