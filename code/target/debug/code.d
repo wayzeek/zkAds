cc72133d808f41d55bfa20d95c120408a3051e07
AntoineF4C5 <antoinedcht@hotmail.com>
2024-08-10 20:39:20 -0400

    added code/target to gitignore

--- code/target/debug/code.d
+++
@@ -1,1 +0,0 @@
-/home/vcart/own_sh*t/zkHack/zkAds/code/target/debug/code: /home/vcart/own_sh*t/zkHack/zkAds/code/src/main.rs
--- code/target/debug/deps/code-f2251c457d0032f7.d
+++
@@ -1,5 +0,0 @@
-/home/antoine/Documents/projets/dev/zkHack/project/zkAds/code/target/debug/deps/libcode-f2251c457d0032f7.rmeta: src/main.rs
-
-/home/antoine/Documents/projets/dev/zkHack/project/zkAds/code/target/debug/deps/code-f2251c457d0032f7.d: src/main.rs
-
-src/main.rs:
